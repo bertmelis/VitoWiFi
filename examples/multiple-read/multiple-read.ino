276907b4bfe64a8eae0e506dd2e847b7efb8897e
Bert Melis <bert_melis@hotmail.com>
2017-04-06 10:52:08 +0200

    Add multiple-read example
    + fix resetting lastMillis

--- examples/multiple-read/multiple-read.ino
+++ examples/multiple-read/multiple-read.ino
@@ -23,8 +23,17 @@ bool getValues = false;
 //Use struct to hold Viessmann datapoints
 //name - RW - address - length - type
 //char[15+1] - READ/WRITE - uint16_t - uint8_t - type
-Datapoint DP = { "OutsideTemp", READ, 0x5525, 16, TEMP};
-
+Datapoint DP[] = {
+  "toutside", READ, 0x5525, 2, TEMP,
+  "tdhw", READ, 0x0812, 2, TEMP,
+  "tdhwsolar", READ, 0x6566, 2, TEMP,
+  "tdhwsolarcoll", READ, 0x6564, 2, TEMP,
+  "statsolarpump", READ, 0x0846, 1, TEMP,
+  "tboiler", READ, 0x0810, 2, TEMP,
+  "tfluegas", READ, 0x5642, 2, TEMP
+};
+uint8_t numberOfDPs = sizeof(DP) / sizeof(DP[0]);
+uint8_t currentDP = 0;
 
 void setup(){
   WiFi.mode(WIFI_STA);
@@ -43,19 +52,24 @@ void loop(){
 
   if((millis() - lastMillis) > 30000){
     //flag to get new values every 30 seconds
+    //start with DP zero.
     lastMillis = millis();
     getValues = true;
+    currentDP = 0;
   }
 
   //if flag is set, get values
   if(getValues){
     //pass Datapoint by reference
-    myVitoWifi.sendDP(&DP);
-    getValues = false;
+    myVitoWifi.sendDP(&DP[currentDP]);
+    //stop sending new DPs when all DPs are sent.
+    if(currentDP >= numberOfDPs) getValues = false;
   }
 
   //when value is available, display
   if( myVitoWifi.getStatus() == RETURN ){
+      //Display value and move to next DP
+      currentDP++;
       myVitoWifi.getLogger().print("Name: ");
       myVitoWifi.getLogger().println(DP.name);
       myVitoWifi.getLogger().print("Value: ");
