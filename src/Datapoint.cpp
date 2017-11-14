98259d86e18f0e092f19f80145436f6790c28567
tolw <siodmok@op.pl>
2017-11-14 21:31:52 +0100

    Extend the char array to allow multi digit  values (#8)
    * Extend the char array to allow multi digit  values
    
    * Add comments

--- src/Datapoint.cpp
+++ src/Datapoint.cpp
@@ -200,7 +200,8 @@ void ModeDP::callback(uint8_t value[]) {
     _callback(_name, _group, value[0]);
   }
   else if (_globalCallback) {
-    char str[2] = {'\0'};
+    char str[4] = {'\0'};  //4 instead of 2 to allow reuse DP for setting temp to 1-byte-temp-dp
+                           //to do: add and rearrange DPs
     snprintf(str, sizeof(str), "%u", value[0]);
     _globalCallback(_name, _group, str);
   }
