f4faf16d51d930371b42aa45bdb84a14b014f9ef
bertmelis <bertmelis@users.noreply.github.com>
2019-03-12 20:42:07 +0100

    Update travis.sh

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -7,7 +7,7 @@ then
   cpplint --repository=. --recursive --linelength=200 --filter=-build/include  ./src
 elif [[ "$TESTQUEUE" ]]
 then
-  platformio ci --lib="." --project-conf=../tests/platformio.ini --build-dir=build --keep-build-dir ../tests/queue.cpp
+  platformio ci --lib="./src" --project-conf="./tests/platformio.ini" --build-dir=build --keep-build-dir ./tests/queue.cpp
 else
   echo "should be building examples"
   # platformio ci --lib="." $PLATFORMIO_CI_EXTRA_ARGS
