ac3ee3901d13fcbc7e049e2a0727ff388272a445
bertmelis <bertmelis@users.noreply.github.com>
2019-03-12 21:22:02 +0100

    fix Travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -4,12 +4,18 @@ echo "Starting Travis job"
 
 if [[ "$CPPLINT" ]]
 then
+  pip install -U cpplint
   cpplint --repository=. --recursive --linelength=200 --filter=-build/include  ./src
 elif [[ "$TESTQUEUE" ]]
 then
+  pip install -U platformio
+  platformio update
   mkdir build
   platformio ci --lib="./src" --project-conf="tests/platformio.ini" --build-dir=build --keep-build-dir tests/queue.cpp
 else
+  pip install -U platformio
+  platformio update
+  platformio lib -g install 416 # Blynk
   echo "should be building examples"
   # platformio ci --lib="." $PLATFORMIO_CI_EXTRA_ARGS
 fi
