ff3c814094b1f1b2b90678d251f4053493670bdb
bertmelis <bertmelis@users.noreply.github.com>
2019-03-25 21:33:33 +0100

    install pio

--- scripts/build_examples.sh
+++ scripts/build_examples.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#pip install -U platformio
-#platformio update
-#pip install -U platformio
-#platformio update
-#platformio lib -g install 416  # Blynk
-#platformio lib -g install 305  # ESPAsyncTCP
-#platformio lib -g install 1826 # AsyncTCP
-#platformio lib -g install 306  # ESP Async Webserver
-#platformio lib -g install 1106 # Bounce2
-#platformio lib -g install 555  # Homie
+pip install -U platformio
+platformio update
+pip install -U platformio
+platformio update
+platformio lib -g install 416  # Blynk
+platformio lib -g install 305  # ESPAsyncTCP
+platformio lib -g install 1826 # AsyncTCP
+platformio lib -g install 306  # ESP Async Webserver
+platformio lib -g install 1106 # Bounce2
+platformio lib -g install 555  # Homie
 
 find ./examples/ -maxdepth 1 -mindepth 1 -type d -print0 | while IFS= read -r -d $'\0' line; do
   if [[ -e "$line/platformio.ini" ]]
