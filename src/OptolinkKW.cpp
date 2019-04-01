9aa0d26430c3283a8471988547c2762cf1369ba0
bertmelis <bertmelis@users.noreply.github.com>
2019-04-01 12:27:01 +0200

    merge KW

--- src/OptolinkKW.cpp
+++ src/OptolinkKW.cpp
@@ -33,11 +33,11 @@ inline void clearInput(HardwareSerial* serial) {
 
 OptolinkKW::OptolinkKW(HardwareSerial* serial) :
   Optolink(serial),
-  _state(RESET) {}
-
-OptolinkKW::~OptolinkKW() {
-  // TODO(bertmelis): anything to do?
-}
+  _state(RESET),
+  _lastMillis(0),
+  _rcvBuffer{0},
+  _rcvBufferLen(0),
+  _rcvLen(0) {}
 
 void OptolinkKW::begin() {
   _serial->begin(4800, SERIAL_8E2);
@@ -46,6 +46,103 @@ void OptolinkKW::begin() {
 
 void OptolinkKW::loop() {
   // TODO(bertmelis)
+}
+
+void OptolinkKW::_init() {
+  if (_serial->available()) {
+    if (_serial->peek() == 0x05) {
+      _state = IDLE;
+      _idle();
+    } else {
+      _serial->read();
+    }
+  } else {
+    if (millis() - _lastMillis > 1000UL) {  // reset should Vitotronic be
+                                            // connected (P300)
+      _lastMillis = millis();
+      const uint8_t buff[] = {0x04};
+      _serial->write(buff, sizeof(buff));
+    }
+  }
+}
+
+void OptolinkKW::_idle() {
+  if (_serial->available()) {
+    if (_serial->read() == 0x05) {
+      _lastMillis = millis();
+      if (_queue.size() > 0) {
+        _state = SYNC;
+      }
+    } else {
+      // received something unexpected
+    }
+  } else if (_queue.size() > 0 && (millis() - _lastMillis < 10UL)) {
+    // don't wait for 0x05 sync signal, send directly after last request
+    _state = SEND;
+  } else if (millis() - _lastMillis > 5 * 1000UL) {
+    _state = INIT;
+  }
+}
+
+void OptolinkKW::_sync() {
+  const uint8_t buff[1] = {0x01};
+  _serial->write(buff, sizeof(buff));
+  _state = SEND;
+  _send();
+}
+
+//
+void OptolinkKW::_send() {
+  uint8_t buff[MAX_DP_LENGTH + 4];
+  OptolinkDP* dp = _queue.front();
+  uint8_t length = dp->length;
+  uint16_t address = dp->address;
+  if (dp->write) {
+    // type is WRITE
+    // has length of 4 chars + length of value
+    buff[0] = 0xF4;
+    buff[1] = (address >> 8) & 0xFF;
+    buff[2] = address & 0xFF;
+    buff[3] = length;
+    // add value to message
+    memcpy(&buff[4], dp->data, length);
+    _rcvLen = 1;  // expected length is only ACK (0x00)
+    _serial->write(buff, 4 + length);
+  } else {
+    // type is READ
+    // has fixed length of 4 chars
+    buff[0] = 0xF7;
+    buff[1] = (address >> 8) & 0xFF;
+    buff[2] = address & 0xFF;
+    buff[3] = length;
+    _rcvLen = length;  // expected answer length the same as sent
+    _serial->write(buff, 4);
+  }
+  _rcvBufferLen = 0;
+  _state = RECEIVE;
+  _lastMillis = millis();
+}
+
+void OptolinkKW::_receive() {
+  while (_serial->available() > 0) {  // while instead of if: read complete RX buffer
+    _rcvBuffer[_rcvBufferLen] = _serial->read();
+    ++_rcvBufferLen;
+  }
+  if (_rcvBufferLen == _rcvLen) {  // message complete, TODO: check message (eg 0x00 for READ messages)
+    _state = IDLE;
+    _lastMillis = millis();
+    if (_queue.front()->write) {
+      _tryOnData(_queue.front()->data, _queue.front()->length);
+    } else {
+      _tryOnData(_rcvBuffer, _rcvBufferLen);
+    }
+    return;
+  } else if (millis() - _lastMillis > 1 * 1000UL) {  // Vitotronic isn't answering, try again
+    _tryOnError(TIMEOUT);
+    _rcvBufferLen = 0;
+    memset(_rcvBuffer, 0, MAX_DP_LENGTH);
+    _state = INIT;
+  }
 }
 
 #elif defined VITOWIFI_TEST
--- src/OptolinkKW.h
+++ src/OptolinkKW.h
@@ -23,6 +23,14 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
 
+/**
+ * @file OptolinkKW.h
+ * @brief Optolink API definions (protocol KW).
+ *
+ * This file contains all method definitions for the Optolink for the 
+ * protocol KW.
+ */
+
 #pragma once
 
 #if defined ARDUINO_ARCH_ESP8266 || ARDUINO_ARCH_ESP32
@@ -30,17 +38,55 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "Optolink.h"
 #include <Arduino.h>  // for millis
 
+/**
+ * @brief Protocol implementation class for the Optolink (KW).
+ * 
+ * This class is a KW version of the Optolink.
+ */
 class OptolinkKW : public Optolink {
  public:
+  /**
+   * @brief Construct the Optolink object (KW)
+   * 
+   * @param serial Hardwareserial object to be used. Pass by reference.
+   */
   explicit OptolinkKW(HardwareSerial* serial);
-  ~OptolinkKW();
+
+  /**
+   * @brief Method to start the Optolink.
+   * 
+   * Calling this method starts the Hardwareserial (passed in the constructor)
+   * with the right settings.
+   */
   void begin();
+
+  /**
+   * @brief Method to keep the Optolink running.
+   * 
+   * This methid has to be called frequently. The Optolink object works by 
+   * polling the Hardwareserial. If applicable, add a delay between calls to 
+   * feed a watchdog (needed for ESP8266 and ESP32).
+   */
   void loop();
 
  private:
   enum OptolinkState : uint8_t {
-    RESET = 0
+    RESET = 0,
+    INIT,
+    IDLE,
+    SYNC,
+    SEND,
+    RECEIVE
   } _state;
+  void _init();
+  void _idle();
+  void _sync();
+  void _send();
+  void _receive();
+  uint32_t _lastMillis;
+  uint8_t _rcvBuffer[MAX_DP_LENGTH];
+  uint8_t _rcvBufferLen;
+  uint8_t _rcvLen;
 };
 
 #elif defined VITOWIFI_TEST
